f3b20c74052b048a8fc750640dff311eaadae34a
Orangestar <orangestar@cats4gold.net>
2015-02-27 17:50:48 -0600

    Initial commit
    Unlike LLMA I didn't screw up and add the wrong folder!!

---
+++ Snapper/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace snapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notif_icon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notification_contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showOptionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.startSnappingNowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseSnappingTemporarilyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitSnapperToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hrsbox = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.minbox = new System.Windows.Forms.NumericUpDown();
+            this.secbox = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dir_text = new System.Windows.Forms.TextBox();
+            this.quitbtn = new System.Windows.Forms.Button();
+            this.startbtn = new System.Windows.Forms.Button();
+            this.timeout_help = new System.Windows.Forms.Button();
+            this.save_help = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.stopSnappingMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.notification_contextMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hrsbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secbox)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notif_icon
+            // 
+            this.notif_icon.ContextMenuStrip = this.notification_contextMenu;
+            this.notif_icon.Icon = ((System.Drawing.Icon)(resources.GetObject("notif_icon.Icon")));
+            this.notif_icon.Text = "Snapper";
+            this.notif_icon.Visible = true;
+            this.notif_icon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notif_icon_MouseClick);
+            // 
+            // notification_contextMenu
+            // 
+            this.notification_contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showOptionsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.startSnappingNowToolStripMenuItem,
+            this.pauseSnappingTemporarilyToolStripMenuItem,
+            this.stopSnappingMenuItem1,
+            this.toolStripSeparator2,
+            this.exitSnapperToolStripMenuItem});
+            this.notification_contextMenu.Name = "contextMenuStrip1";
+            this.notification_contextMenu.Size = new System.Drawing.Size(222, 148);
+            // 
+            // showOptionsToolStripMenuItem
+            // 
+            this.showOptionsToolStripMenuItem.Name = "showOptionsToolStripMenuItem";
+            this.showOptionsToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.showOptionsToolStripMenuItem.Text = "Show options";
+            this.showOptionsToolStripMenuItem.Click += new System.EventHandler(this.showOptionsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(218, 6);
+            // 
+            // startSnappingNowToolStripMenuItem
+            // 
+            this.startSnappingNowToolStripMenuItem.Name = "startSnappingNowToolStripMenuItem";
+            this.startSnappingNowToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.startSnappingNowToolStripMenuItem.Text = "Start snapping now";
+            this.startSnappingNowToolStripMenuItem.Click += new System.EventHandler(this.startbtn_Click);
+            // 
+            // pauseSnappingTemporarilyToolStripMenuItem
+            // 
+            this.pauseSnappingTemporarilyToolStripMenuItem.Name = "pauseSnappingTemporarilyToolStripMenuItem";
+            this.pauseSnappingTemporarilyToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.pauseSnappingTemporarilyToolStripMenuItem.Text = "Pause snapping temporarily";
+            this.pauseSnappingTemporarilyToolStripMenuItem.Click += new System.EventHandler(this.pauseSnappingTemporarilyToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(218, 6);
+            // 
+            // exitSnapperToolStripMenuItem
+            // 
+            this.exitSnapperToolStripMenuItem.Name = "exitSnapperToolStripMenuItem";
+            this.exitSnapperToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.exitSnapperToolStripMenuItem.Text = "Exit Snapper";
+            this.exitSnapperToolStripMenuItem.Click += new System.EventHandler(this.quitbtn_Click);
+            // 
+            // hrsbox
+            // 
+            this.hrsbox.Location = new System.Drawing.Point(18, 32);
+            this.hrsbox.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.hrsbox.Name = "hrsbox";
+            this.hrsbox.Size = new System.Drawing.Size(45, 20);
+            this.hrsbox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hours";
+            // 
+            // minbox
+            // 
+            this.minbox.Location = new System.Drawing.Point(69, 32);
+            this.minbox.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.minbox.Name = "minbox";
+            this.minbox.Size = new System.Drawing.Size(45, 20);
+            this.minbox.TabIndex = 3;
+            // 
+            // secbox
+            // 
+            this.secbox.Location = new System.Drawing.Point(120, 32);
+            this.secbox.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.secbox.Name = "secbox";
+            this.secbox.Size = new System.Drawing.Size(45, 20);
+            this.secbox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Minutes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Seconds";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Snapshot Interval";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.hrsbox);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.minbox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.secbox);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(12, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(187, 71);
+            this.panel1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Save Location";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.dir_text);
+            this.panel2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel2.Location = new System.Drawing.Point(13, 137);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(187, 60);
+            this.panel2.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Change Directory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dir_text
+            // 
+            this.dir_text.BackColor = System.Drawing.SystemColors.Window;
+            this.dir_text.Location = new System.Drawing.Point(3, 3);
+            this.dir_text.Name = "dir_text";
+            this.dir_text.ReadOnly = true;
+            this.dir_text.Size = new System.Drawing.Size(179, 20);
+            this.dir_text.TabIndex = 10;
+            // 
+            // quitbtn
+            // 
+            this.quitbtn.Location = new System.Drawing.Point(3, 210);
+            this.quitbtn.Name = "quitbtn";
+            this.quitbtn.Size = new System.Drawing.Size(60, 23);
+            this.quitbtn.TabIndex = 10;
+            this.quitbtn.Text = "Quit";
+            this.quitbtn.UseVisualStyleBackColor = true;
+            this.quitbtn.Click += new System.EventHandler(this.quitbtn_Click);
+            // 
+            // startbtn
+            // 
+            this.startbtn.Location = new System.Drawing.Point(150, 210);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(58, 23);
+            this.startbtn.TabIndex = 11;
+            this.startbtn.Text = "Start";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.startbtn_Click);
+            this.startbtn.MouseEnter += new System.EventHandler(this.start_notify);
+            // 
+            // timeout_help
+            // 
+            this.timeout_help.Location = new System.Drawing.Point(176, 4);
+            this.timeout_help.Name = "timeout_help";
+            this.timeout_help.Size = new System.Drawing.Size(24, 23);
+            this.timeout_help.TabIndex = 12;
+            this.timeout_help.Text = "?";
+            this.timeout_help.UseVisualStyleBackColor = true;
+            this.timeout_help.Click += new System.EventHandler(this.timeout_help_Click);
+            // 
+            // save_help
+            // 
+            this.save_help.Location = new System.Drawing.Point(176, 108);
+            this.save_help.Name = "save_help";
+            this.save_help.Size = new System.Drawing.Size(24, 23);
+            this.save_help.TabIndex = 13;
+            this.save_help.Text = "?";
+            this.save_help.UseVisualStyleBackColor = true;
+            this.save_help.Click += new System.EventHandler(this.save_help_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(77, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(59, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_Click);
+            // 
+            // stopSnappingMenuItem1
+            // 
+            this.stopSnappingMenuItem1.Name = "stopSnappingMenuItem1";
+            this.stopSnappingMenuItem1.Size = new System.Drawing.Size(221, 22);
+            this.stopSnappingMenuItem1.Text = "Stop Snapping";
+            this.stopSnappingMenuItem1.Click += new System.EventHandler(this.stopSnappingMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(211, 245);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.save_help);
+            this.Controls.Add(this.timeout_help);
+            this.Controls.Add(this.startbtn);
+            this.Controls.Add(this.quitbtn);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Snapper";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.notification_contextMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.hrsbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secbox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip notification_contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem showOptionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startSnappingNowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseSnappingTemporarilyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitSnapperToolStripMenuItem;
+        private System.Windows.Forms.NumericUpDown hrsbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown minbox;
+        private System.Windows.Forms.NumericUpDown secbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox dir_text;
+        private System.Windows.Forms.Button quitbtn;
+        private System.Windows.Forms.Button startbtn;
+        private System.Windows.Forms.Button timeout_help;
+        private System.Windows.Forms.Button save_help;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.NotifyIcon notif_icon;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem stopSnappingMenuItem1;
+    }
+}
+
---
+++ Snapper/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace snapper
+{
+    public partial class Form1 : Form
+    {
+        private bool firsttime = true;
+        private bool firstclick = true;
+        private string directory = @"C:\";
+        private int interval;
+        private int imgs;
+        private bool stop = false;
+        private Timer time = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XmlDocument optionsfile = new XmlDocument();
+            optionsfile.LoadXml(File.OpenText("options.xml").ReadToEnd());
+            XmlNodeList options = optionsfile.SelectNodes("/options/time");
+
+            hrsbox.Value = Decimal.Parse(options[0]["hours"].InnerText);
+            minbox.Value = Decimal.Parse(options[0]["minutes"].InnerText);
+            secbox.Value = Decimal.Parse(options[0]["seconds"].InnerText);
+
+            XmlNode directoryxml = optionsfile.SelectSingleNode("/options/directory");
+            dir_text.Text = directoryxml.InnerText;
+            directory = directoryxml.InnerText;
+
+            notif_icon.BalloonTipTitle = "Click to Minimize";
+            notif_icon.BalloonTipText = "After changing the settings, you can click here or click the red [X] to minimize Snapper down here. Or, just click Start.";
+            notif_icon.ShowBalloonTip(2000);
+        }
+
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+
+            notif_icon.BalloonTipTitle = "Snapper's down here!";
+            notif_icon.BalloonTipText = "Right click this icon for a nifty menu, or just click it to show the options again.";
+
+            this.Hide();
+
+            if (firsttime == true)
+            {
+                notif_icon.ShowBalloonTip(1000);
+                firsttime = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog newDir = new FolderBrowserDialog();
+            newDir.Description = "Select the folder you would like Snapper to save your screenshots to.";
+            newDir.ShowDialog();
+            directory = newDir.SelectedPath;
+            dir_text.Text = directory;
+        }
+
+        private void timeout_help_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is the amount of time Snapper will wait inbetween each snapshot.\r\n\r\nSnapper will try it's best, but limitations in the .NET framework mean that your image might not be taken at _exactly_ the time entered in these boxes. However, it will probably be close enough that it shouldn't matter.");
+        }
+
+        private void save_help_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is where Snapper will save each snapshot.\r\n\r\nNote that you should have write-permission to this directory, otherwise you will have to run Snapper as an administrator.");
+        }
+
+        private void quitbtn_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void showOptionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void notif_icon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (this.Visible)
+                {
+                    this.Hide();
+                }
+                else
+                {
+                    this.Show();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                FileStream optionsstream = new FileStream("options.xml", FileMode.Open);
+                StreamReader optionsdoc = new StreamReader(optionsstream, System.Text.Encoding.UTF8);
+                XmlDocument optionsfile = new XmlDocument();
+                optionsfile.LoadXml(optionsdoc.ReadToEnd());
+                XmlNodeList options = optionsfile.SelectNodes("/options/time");
+
+                options[0]["hours"].InnerText = hrsbox.Value.ToString();
+                options[0]["minutes"].InnerText = minbox.Value.ToString();
+                options[0]["seconds"].InnerText = secbox.Value.ToString();
+
+                XmlNode directoryxml = optionsfile.SelectSingleNode("/options/directory");
+                directoryxml.InnerText = directory;
+                optionsstream.SetLength(0);
+                optionsfile.Save(optionsstream);
+                optionsstream.Close();
+            }
+            else if(e.Button == MouseButtons.Right)
+            {
+                MessageBox.Show("This will save the options you have set to the XML file in the Snapper directory, which will then be loaded each time you start Snapper.");
+            }
+        }
+
+        private void start_notify(object sender, EventArgs e)
+        {
+            if (firstclick)
+            {
+                notif_icon.ShowBalloonTip(500, "Before you start!", "Snapper will take a \"Preliminary Screenshot\" 3 seconds after the program starts. Make sure that you're all set before you click!", ToolTipIcon.Error);
+                firstclick = false;
+            }
+        }
+
+        private void startbtn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            if (stop)
+            {
+                imgs = 1;
+                stop = false;
+                interval = (int)((hrsbox.Value * 3600000) + (minbox.Value * 60000) + (secbox.Value * 1000));
+                notif_icon.ShowBalloonTip(0, "Snapper is starting!", "Snapper will take its first screenshot in 3 seconds...", ToolTipIcon.Info);
+                System.Threading.Thread.Sleep(3000);
+                cap();
+                time.Interval = interval;
+                time.Tick += new EventHandler(cap);
+                time.Start();
+            }
+        }
+
+        private void cap()
+        {
+            if (stop)
+            {
+                time.Stop();
+            }
+            else
+            {
+                // http://stackoverflow.com/questions/5049122/capture-the-screen-shot-using-net
+                Bitmap bmpScreenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                                     Screen.PrimaryScreen.Bounds.Height);
+                Graphics g = Graphics.FromImage(bmpScreenCapture);
+                g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                 Screen.PrimaryScreen.Bounds.Y,
+                                 0, 0,
+                                 bmpScreenCapture.Size,
+                                 CopyPixelOperation.SourceCopy);
+
+                bmpScreenCapture.Save(directory + "\\snapper_" + imgs + ".png");
+                notif_icon.BalloonTipTitle = "Captured!";
+                notif_icon.BalloonTipText = "Snapper has captured image #" + imgs + ".";
+                notif_icon.ShowBalloonTip(0);
+                imgs++;
+            }
+        }
+        private void cap(object sender, EventArgs e) { cap(); }
+
+        private void stopSnappingMenuItem1_Click(object sender, EventArgs e)
+        {
+            stop = true;
+            pauseSnappingTemporarilyToolStripMenuItem.Checked = false;
+        }
+
+        private void pauseSnappingTemporarilyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!stop)
+            {
+                if (pauseSnappingTemporarilyToolStripMenuItem.Checked)
+                {
+                    time.Enabled = true;
+                    pauseSnappingTemporarilyToolStripMenuItem.Checked = false;
+                }
+                else
+                {
+                    time.Enabled = false;
+                    pauseSnappingTemporarilyToolStripMenuItem.Checked = true;
+                }
+            }
+        }
+    }
+}
